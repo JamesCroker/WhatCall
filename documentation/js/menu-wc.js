43eac14d9446f62dae0c650f4699c2272cd4860d
JamesCroker <mcroker@gmail.com>
2026-05-09 13:49:06 +0100

    add compodoc

---
+++ documentation/js/menu-wc.js
@@ -0,0 +1,192 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">whatcall documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                                <li class="link">
+                                    <a href="overview.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-keypad"></span>Overview
+                                    </a>
+                                </li>
+
+                            <li class="link">
+                                <a href="index.html" data-type="chapter-link">
+                                    <span class="icon ion-ios-paper"></span>
+                                        README
+                                </a>
+                            </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/App.html" data-type="entity-link" >App</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AuthScreenComponent.html" data-type="entity-link" >AuthScreenComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HomeComponent.html" data-type="entity-link" >HomeComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/MenuComponent.html" data-type="entity-link" >MenuComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ResponsesChartComponent.html" data-type="entity-link" >ResponsesChartComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ScenarioComponent.html" data-type="entity-link" >ScenarioComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/UploadComponent.html" data-type="entity-link" >UploadComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/VideoPlayerComponent.html" data-type="entity-link" >VideoPlayerComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/AuthModalService.html" data-type="entity-link" >AuthModalService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/ProfileService.html" data-type="entity-link" >ProfileService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/ScenarioPageController.html" data-type="entity-link" >ScenarioPageController</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/ScenarioService.html" data-type="entity-link" >ScenarioService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/StorageService.html" data-type="entity-link" >StorageService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/UploadModalService.html" data-type="entity-link" >UploadModalService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/FirebaseConfig.html" data-type="entity-link" >FirebaseConfig</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/FirebaseEmulatorConfig.html" data-type="entity-link" >FirebaseEmulatorConfig</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Profile.html" data-type="entity-link" >Profile</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Scenario.html" data-type="entity-link" >Scenario</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Scenario-1.html" data-type="entity-link" >Scenario</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ScenarioResponse.html" data-type="entity-link" >ScenarioResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ScenarioResponse-1.html" data-type="entity-link" >ScenarioResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ScenarioStats.html" data-type="entity-link" >ScenarioStats</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ScenarioStats-1.html" data-type="entity-link" >ScenarioStats</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ScenarioWithResponses.html" data-type="entity-link" >ScenarioWithResponses</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ScenarioWithResponses-1.html" data-type="entity-link" >ScenarioWithResponses</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+                        </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
